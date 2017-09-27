80e0a79426003ca2f20c62f506fb507a1d06949c
da1234 <darrell.adjei@hotmail.com>
2017-09-27 23:28:58 +0100

    Update muons2.py

--- muons2.py
+++ muons2.py
@@ -9,39 +9,25 @@ import scipy.special as sp
 from muons1 import B1
 from scipy.optimize import curve_fit
 
-
-
-def std_dependence():
-    
-    
+def std_dependence():        
     N = np.arange(5,10001,1)
     err = np.zeros(N.shape)
-
-    for i in range(len(N)):
-    
+    for i in range(len(N)):    
         x = B1()
         x.read(N[i])
-
-        err[i] = x.derive_std(x.NLL,[0.3,0.4,0.5],0.3)
-        
+        err[i] = x.derive_std(x.NLL,[0.3,0.4,0.5],0.3)        
     plt.figure()
     plt.title("std N dependence")
     plt.xlabel("Number of pairs")
     plt.ylabel("error, (picoseconds)")
     plt.ylim(ymax=0.2)
-    plt.ylim(ymin=-0.01)
-    
+    plt.ylim(ymin=-0.01)    
     plt.axhline(y=0.001)
     plt.plot(N,err,'*')
-
-        
+       
     def ffunc2(x, a):
-        return a/np.sqrt(x)
-        
-    popt, pcov = curve_fit(ffunc2, N, err, p0=(1))
-    
-
-    print "this is popt",popt,"this is pcov", pcov
+        return a/np.sqrt(x)        
+    popt, pcov = curve_fit(ffunc2, N, err, p0=(1)) 
     nN =np.arange(5,300000)
     plt.plot(nN, ffunc2(nN,popt[0]))
     plt.grid()
